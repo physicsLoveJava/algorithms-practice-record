d8d042c0797ec85c5d32a43cb0a61e69f2c37f99
lujian <lujian282012@163.com>
2017-11-18 23:17:21 +0800

    add @offer modify utilities

--- src/main/java/util/FunctionalUtils.java
+++ src/main/java/util/FunctionalUtils.java
@@ -1,28 +1,64 @@
 package util;
 
-import util.adt.LinkNode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FunctionalUtils {
 
-    public static <T> T[] map(T[] arr, Callback<T> callback) {
+    public static <T, S> S[] map(T[] arr, Callback<T, S> callback, S[] typeS) {
         if(arr == null) {
             return null;
         }
-        T[] result = (T[]) new Object[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            result[i] = callback.each(arr[i], i, arr);
+        List<T> arrList = Arrays.asList(arr);
+        List<S> tarList = new ArrayList<S>();
+        for (int i = 0; i < arrList.size(); i++) {
+            tarList.add(callback.each(arrList.get(i), i, arr));
         }
-        return result;
+        return tarList.toArray(typeS);
     }
 
+    public static <T, S> S reduce(T[] arr, ReduceCallback<T, S> callback, S initVal) {
+        if(arr == null) {
+            return null;
+        }
+        if(initVal == null) {
+            throw new IllegalArgumentException("init value should be accessible");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            initVal = callback.reduce(initVal, arr[i], arr);
+        }
+        return initVal;
+    }
 
+    public static <T, S> S join(T[] arr, JoinCallback<T, S> callback, S initValue) {
+        if(arr == null) {
+            return null;
+        }
+        S initVal = initValue;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if(initVal == null) {
+                initVal = callback.join(arr[i], arr[i + 1]);
+            }else {
+                initVal = callback.merge(initVal, callback.join(arr[i], arr[i + 1]));
+            }
+        }
+        return initVal;
+    }
 
     // interface
-
-    public interface Callback<T>{
-        T each(T item, int index, T[] arr);
+    public interface Callback<T, S> {
+        S each(T item, int index, T[] arr);
     }
 
+    public interface JoinCallback<T, S> {
+        S join(T prev, T next);
 
+        S merge(S initVal, S cur);
+    }
+
+    public interface ReduceCallback<T, S> {
+        S reduce(S cur, T next, T[] arr);
+    }
 
 }
--- src/main/java/util/PrintUtils.java
+++ src/main/java/util/PrintUtils.java
@@ -1,4 +1,37 @@
 package util;
 
+import com.google.common.base.Joiner;
+
+import static util.FunctionalUtils.map;
+
 public class PrintUtils {
+
+    private static <T> String internalPrint(T[] arr) {
+        return internalPrintWithCallback(arr, new FunctionalUtils.Callback<T,String>() {
+            @Override
+            public String each(T item, int index, T[] arr) {
+                return item.toString();
+            }
+        });
+    }
+
+    private static <T> String internalPrintWithCallback(T[] arr, FunctionalUtils.Callback<T, String> cb) {
+        return Joiner.on(" -> ").join(map(arr, cb, new String[0]));
+    }
+
+    public static <T> void print(T[] arr) {
+        System.out.print(internalPrint(arr));
+    }
+
+    public static <T> void println(T[] arr) {
+        System.out.println(internalPrint(arr));
+    }
+
+    public static <T> void print(T[] arr, FunctionalUtils.Callback<T, String> cb) {
+        System.out.print(internalPrintWithCallback(arr, cb));
+    }
+
+    public static <T> void println(T[] arr, FunctionalUtils.Callback<T, String> cb) {
+        System.out.println(internalPrintWithCallback(arr, cb));
+    }
 }
