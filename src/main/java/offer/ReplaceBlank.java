5dab4ebf292574a59197dc1358f5c36a6e71e6a0
lujian <lujian282012@163.com>
2017-11-18 17:23:13 +0800

    add @offer replace-blank rename function

--- src/main/java/offer/ReplaceBlank.java
+++ src/main/java/offer/ReplaceBlank.java
@@ -7,7 +7,7 @@ public class ReplaceBlank {
             return null;
         }
         char[] array = str.toCharArray();
-        int blanks = getBlank(array);
+        int blanks = getBlankCounts(array);
         char[] nArray = new char[array.length + 2 * blanks];
         for (int i = 0, j = 0; i < array.length; i++) {
             if(array[i] == ' ') {
@@ -21,7 +21,7 @@ public class ReplaceBlank {
         return new String(nArray);
     }
 
-    private static int getBlank(char[] array) {
+    private static int getBlankCounts(char[] array) {
         if(array == null) {
             return 0;
         }
