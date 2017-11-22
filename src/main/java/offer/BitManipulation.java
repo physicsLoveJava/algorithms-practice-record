2f89993b7c6efd5905133ece6076dab35e28cafd
lujian <lujian282012@163.com>
2017-11-22 22:10:25 +0800

    add @offer @bit-manipulation

---
+++ src/main/java/offer/BitManipulation.java
@@ -0,0 +1,18 @@
+package offer;
+
+public class BitManipulation {
+
+    public static void main(String[] args) {
+
+        System.out.println("2 << 2 : " + (2 << 2));
+        System.out.println("2 >> 2 : " + (2 >> 2));
+        System.out.println("-2 << 2 : " + (-2 << 2));
+        System.out.println("-2 >> 1 : " + (-2 >> 1));
+        System.out.println("-2 >> 2 : " + (-2 >> 2));
+        System.out.println("-2 >> 3 : " + (-2 >> 3));
+        System.out.println(Integer.toBinaryString(-2));
+        System.out.println(Integer.toBinaryString(-1));
+
+    }
+
+}
