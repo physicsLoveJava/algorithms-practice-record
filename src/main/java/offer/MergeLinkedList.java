9f0575adbc5dc153bbbeb95bc8194022f94925d5
lujian <lujian282012@163.com>
2017-11-26 17:40:46 +0800

    add @offer @merge two linked list

--- src/main/java/offer/MergeLinkedList.java
+++ src/main/java/offer/MergeLinkedList.java
@@ -11,26 +11,12 @@ public class MergeLinkedList {
         if(listB == null) {
             return listA;
         }
-        LinkNode<Integer> p1 = listA;
-        LinkNode<Integer> p2 = listB;
-        LinkNode<Integer> result = null;
-        while(p1 != null && p2 != null) {
-            if(p1.getData() >= p2.getData()) {
-                if(result == null) {
-                    result = p2;
-                }
-                LinkNode<Integer> pN = p2.getNext();
-                p1.setNext(new LinkNode<>(p1.getData(), p1.getNext()));
-                p1.setData(p2.getData());
-                p1 = p1.getNext();
-                p2 = pN;
-            }else {
-                LinkNode<Integer> temp = p1;
-                p1 = p2;
-                p2 = temp;
-            }
+        if(listA.getData() > listB.getData()) {
+            return merge(listB, listA);
+        }else {
+            listA.setNext(merge(listA.getNext(), listB));
+            return listA;
         }
-        return result;
     }
 
 }
--- src/test/java/offer/MergeLinkedListTest.java
+++ src/test/java/offer/MergeLinkedListTest.java
@@ -5,14 +5,27 @@ import util.CreationUtils;
 import util.adt.LinkNode;
 
 import static org.junit.Assert.*;
+import static util.CreationUtils.linkedList;
 
 public class MergeLinkedListTest {
     @Test
     public void merge() throws Exception {
-        LinkNode<Integer> listA = CreationUtils.linkedList(1, 3);
-        LinkNode<Integer> listB = CreationUtils.linkedList(2, 4);
-        LinkNode<Integer> listC = MergeLinkedList.merge(listA, listB);
-        System.out.println(listC);
+        assertEquals(linkedList(1, 7), MergeLinkedList.merge(linkedList(1, 5), linkedList(5, 7)));
+    }
+
+    @Test
+    public void merge2() throws Exception{
+        System.out.println(MergeLinkedList.merge(linkedList(1, 5), linkedList(3, 7)));
+        assertEquals("LinkNode{data=1, " +
+                        "next=LinkNode{data=2, " +
+                        "next=LinkNode{data=3, " +
+                        "next=LinkNode{data=3, " +
+                        "next=LinkNode{data=4, " +
+                        "next=LinkNode{data=4, " +
+                        "next=LinkNode{data=5, " +
+                        "next=LinkNode{data=6, " +
+                        "next=null}}}}}}}}",
+                MergeLinkedList.merge(linkedList(1, 5), linkedList(3, 7)).toString());
     }
 
 }
