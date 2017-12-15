b7e11aab2c6633de79f4a7c21268bf4c3b588f35
lujian <lujian282012@163.com>
2017-12-15 10:14:52 +0800

    add @books @classic ThreeColorFlag

---
+++ src/main/java/books/classic/ThreeColorFlag.java
@@ -0,0 +1,43 @@
+package books.classic;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public class ThreeColorFlag {
+
+    //Blue White Red
+    public static String move(String str) {
+        if(str == null) {
+            return null;
+        }
+        char[] chars = str.toCharArray();
+        int blue = 0, white = 0, red = chars.length - 1;
+        while(white <= red) {
+            if(isBlue(chars[white])) {
+                if(blue == white) {
+                    blue ++;white++;
+                }else {
+                    ArrayUtils.swap(chars, blue++, white++);
+                }
+            }else if(isRed(chars[white])) {
+                ArrayUtils.swap(chars, red--, white);
+            }else {
+                white++;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    private static boolean isBlue(char aChar) {
+        return aChar == 'B';
+    }
+
+    private static boolean isWhite(char aChar) {
+        return aChar == 'W';
+    }
+
+    private static boolean isRed(char aChar) {
+        return aChar == 'R';
+    }
+
+}
---
+++ src/test/java/books/classic/ThreeColorFlagTest.java
@@ -0,0 +1,21 @@
+package books.classic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ThreeColorFlagTest {
+    @Test
+    public void move() throws Exception {
+
+        assertEquals("BWR", ThreeColorFlag.move("RWB"));
+        assertEquals("BBWR", ThreeColorFlag.move("RWBB"));
+        assertEquals("BBBWR", ThreeColorFlag.move("RWBBB"));
+        assertEquals("BBBBWR", ThreeColorFlag.move("RWBBBB"));
+
+        assertEquals("BBBBWWWWR", ThreeColorFlag.move("RWWWWBBBB"));
+        assertEquals("BBBBWWWWRRRR", ThreeColorFlag.move("RRRRWWWWBBBB"));
+
+    }
+
+}
