6edc09567b1be0e0a124144738b4fa2b48f3851c
lujian <lujian282012@163.com>
2017-12-24 16:52:52 +0800

    add @type dp longest increasing sub sequence add show path

--- src/main/java/types/dp/LongestIncreasingSubsequence.java
+++ src/main/java/types/dp/LongestIncreasingSubsequence.java
@@ -1,5 +1,8 @@
 package types.dp;
 
+import org.apache.commons.lang3.ArrayUtils;
+import util.PrintUtils;
+
 public class LongestIncreasingSubsequence {
 
     public static int find(int[] seq) {
@@ -25,6 +28,49 @@ public class LongestIncreasingSubsequence {
             }
         }
         return max;
+    }
+
+    public static int[] findWithSequence(int[] seq) {
+        if(seq == null) {
+            return null;
+        }
+        int[] res = new int[seq.length];
+        int[] path = new int[seq.length];
+        for (int i = 0; i < seq.length; i++) {
+            res[i] = 1;
+        }
+        for (int i = 0; i < seq.length; i++) {
+            for (int j = i + 1; j < seq.length; j++) {
+                if(seq[i] < seq[j]) {
+                    //如果seq[i]在子队列中，则更新res[i]
+                    if(res[j] < res[i] + 1) {
+                        res[j] = res[i] + 1;
+                        path[j] = i;
+                    }
+                }
+            }
+        }
+        int max = 1;
+        int idx = -1;
+        for (int i = 0; i < res.length; i++) {
+            if(max < res[i]) {
+                max = res[i];
+                idx = i;
+            }
+        }
+        showPath(seq, path, idx, max);
+        return path;
+    }
+
+    private static void showPath(int[] seq, int[] path, int idx, int max) {
+        if(max > 0) {
+            if(max == 1) {
+                System.out.printf("%d ", seq[idx]);
+            }else {
+                System.out.printf("%d -> ", seq[idx]);
+            }
+            showPath(seq, path, path[idx], max - 1);
+        }
     }
 
 }
--- src/test/java/types/dp/LongestIncreasingSubsequenceTest.java
+++ src/test/java/types/dp/LongestIncreasingSubsequenceTest.java
@@ -12,4 +12,11 @@ public class LongestIncreasingSubsequenceTest {
         assertEquals(3, LongestIncreasingSubsequence.find(new int[]{1, 2, 1, 1, 6}));
     }
 
+    @Test
+    public void find2() throws Exception {
+        LongestIncreasingSubsequence.findWithSequence(new int[]{1, 2, 1, 3, 5});
+//        assertArrayEquals(4, LongestIncreasingSubsequence.findWithSequence(new int[]{1, 2, 3, 1, 5}));
+//        assertEquals(4, LongestIncreasingSubsequence.findWithSequence(new int[]{1, 2, 1, 3, 5}));
+//        assertEquals(3, LongestIncreasingSubsequence.findWithSequence(new int[]{1, 2, 1, 1, 6}));
+    }
 }
