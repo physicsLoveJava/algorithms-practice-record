1f56159e3990d622b878d0ab6648b6c783fcdaf0
lujian <lujian282012@163.com>
2018-03-15 19:32:22 +0800

    add @leetcode knight probability in chessboard

--- src/main/java/leetcode/Medium/KnightProbabilityinChessboard.java
+++ src/main/java/leetcode/Medium/KnightProbabilityinChessboard.java
@@ -7,43 +7,28 @@ import java.util.Arrays;
  */
 public class KnightProbabilityinChessboard {
 
+    int[][] moves = {{1, 2}, {1, -2}, {2, 1}, {2, -1}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}};
     public double knightProbability(int N, int K, int r, int c) {
-        int[][] dp = new int[N][N];
-        for (int[] ints : dp) {
-            Arrays.fill(ints, 1);
-        }
-        for (int i = 0; i < K; i++) {
-            int[][] dpX = new int[N][N];
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < N; k++) {
-                    for (int[] direct : directs) {
-                        int row = direct[0] + j;
-                        int col = direct[1] + k;
-                        if(isOnBoard(row, col, N)) {
-                            dpX[j][k] += dp[row][col];
-                        }
+        int len = N;
+        double dp0[][] = new double[len][len];
+        for(double[] row : dp0) Arrays.fill(row, 1);
+        for(int l = 0; l < K; l++) {
+            double[][] dp1 = new double[len][len];
+            for(int i = 0; i < len; i++) {
+                for(int j = 0; j < len; j++) {
+                    for(int[] move : moves) {
+                        int row = i + move[0];
+                        int col = j + move[1];
+                        if(isLegal(row, col, len)) dp1[i][j] += dp0[row][col];
                     }
                 }
             }
-            dp = dpX;
+            dp0 = dp1;
         }
-        return dp[r][c] / Math.pow(8, K);
+        return dp0[r][c] / Math.pow(8, K);
     }
-
-    private boolean isOnBoard(int row, int col, int n) {
-        return row >= 0 && row <= n - 1
-                && col >=0 && col <= n - 1;
+    private boolean isLegal(int r, int c, int len) {
+        return r >= 0 && r < len && c >= 0 && c < len;
     }
-
-    static int[][] directs = new int[][] {
-            {1, 2},
-            {1, -2},
-            {-1, -2},
-            {-1, 2},
-            {2, 1},
-            {2, -1},
-            {-2, -1},
-            {-2, 1}
-    };
 
 }
