67ad45dcc1a0c0b2fcbf9c5ffe1cd8eb81b4ac8f
lujian <lujian282012@163.com>
2018-03-22 18:15:50 +0800

    add @leetcode same tree
    Signed-off-by: lujian <lujian282012@163.com>

---
+++ src/main/java/leetcode/Easy/SameTree.java
@@ -0,0 +1,21 @@
+package leetcode.Easy;
+
+import util.adt.TreeNode;
+
+/**
+ * Problem Url: https://leetcode.com/problems/same-tree
+ */
+public class SameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+
+}
