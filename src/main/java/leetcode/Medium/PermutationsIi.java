0bbfbd2fe096cec700dc0dd2c712967f304eeccd
lujian <lujian282012@163.com>
2018-03-26 16:11:21 +0800

    add @leetcode permutations 2
    Signed-off-by: lujian <lujian282012@163.com>

---
+++ src/main/java/leetcode/Medium/PermutationsIi.java
@@ -0,0 +1,37 @@
+package leetcode.Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Problem Url: https://leetcode.com/problems/permutations-ii
+ */
+public class PermutationsIi {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] visited = new boolean[nums.length];
+        permuteUnique(list, new ArrayList<>(), nums, visited, 0);
+        return list;
+    }
+
+    private void permuteUnique(List<List<Integer>> list, List<Integer> sub, int[] nums, boolean[] visited, int idx) {
+        if(idx == nums.length) {
+            list.add(new ArrayList<>(sub));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(visited[i] || i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
+                continue;
+            }
+            visited[i] = true;
+            sub.add(nums[i]);
+            permuteUnique(list, sub, nums, visited,idx + 1);
+            visited[i] = false;
+            sub.remove(sub.size() - 1);
+        }
+    }
+
+}
---
+++ src/test/java/leetcode/Medium/PermutationsIiTest.java
@@ -0,0 +1,15 @@
+package leetcode.Medium;
+
+import org.junit.Test;
+
+public class PermutationsIiTest {
+    @Test
+    public void permuteUnique() throws Exception {
+
+        System.out.println(new PermutationsIi().permuteUnique(new int[]{
+                1, 1, 2
+        }));
+
+    }
+
+}
