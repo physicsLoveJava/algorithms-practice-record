445aa73dda25fbd8bbd9b09c24b21e2d5cda2f96
lujian <lujian282012@163.com>
2018-04-05 13:53:30 +0800

    add @leetcode pyramid transition matrix for later time
    Signed-off-by: lujian <lujian282012@163.com>

---
+++ src/main/java/leetcode/Medium/PyramidTransitionMatrix.java
@@ -0,0 +1,14 @@
+package leetcode.Medium;
+
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/pyramid-transition-matrix/description/
+ */
+public class PyramidTransitionMatrix {
+
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        return false;
+    }
+
+}
