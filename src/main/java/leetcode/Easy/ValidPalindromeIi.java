b7ce795f95fcdd73250a4368c0ab68cdf501d25d
lujian <lujian282012@163.com>
2018-04-10 16:52:19 +0800

    add @leetcode valid palindrome 2
    Signed-off-by: lujian <lujian282012@163.com>

---
+++ src/main/java/leetcode/Easy/ValidPalindromeIi.java
@@ -0,0 +1,29 @@
+package leetcode.Easy;
+
+/**
+ * Problem Url: https://leetcode.com/problems/valid-palindrome-ii
+ */
+public class ValidPalindromeIi {
+
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i, j - 1) || isPalindrome(s, i + 1, j);
+            }
+            i++;j--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int i, int j) {
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
+    }
+
+}
