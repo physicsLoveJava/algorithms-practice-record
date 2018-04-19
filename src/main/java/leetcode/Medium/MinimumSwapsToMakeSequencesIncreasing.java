350159a9da4bfc2d40294dcdf56455f7ae4d3fc8
lujian <lujian282012@163.com>
2018-04-19 10:26:31 +0800

    add @leetcode minimum swaps to make sequences increasing
    Signed-off-by: lujian <lujian282012@163.com>

---
+++ src/main/java/leetcode/Medium/MinimumSwapsToMakeSequencesIncreasing.java
@@ -0,0 +1,17 @@
+package leetcode.Medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/minimum-swaps-to-make-sequences-increasing/description/
+ */
+public class MinimumSwapsToMakeSequencesIncreasing {
+
+    public int minSwap(int[] a, int[] b) {
+        Map<String, Integer> map = new HashMap<>();
+        boolean[] used = new boolean[a.length];
+        return -1;
+    }
+
+}
